2718c029ba3141e5212cf1292b4183b4e26f16e7
Assim Zemouchi <assim.zpr@gmail.com>
2024-12-06 23:23:13 +0100

    [TCP/UDP] File skeleton

---
+++ client_tcp.cpp
@@ -0,0 +1,35 @@
+/*
+ * Code du client
+ */
+
+#define SIZE 100
+#define PORT 9600
+
+int main(int argc, char *argv[]) {
+
+  /*
+   * Variables du client
+   *
+   * Déclarer ici les variables suivantes :
+   * - stockfd le descripteur de socket
+   * - structure d'adresse du serveur
+   * - pointeur vers la structure descriptive de machine (hostent)
+   * - zone de mémoire destinée à accueillir la chaîne
+   *   entrée au clavier
+   * - taille de la chaîne à envoyer
+   *
+   */
+
+  /*
+   * Code du client
+   *
+   * - Ouvrir le socket du client
+   * - Récupérer l'adresse IP du serveur à partir de son nom
+   *   donné en ligne de commande
+   * - Remplir la structure d'adresse du serveur
+   * - Lire une ligne de l'entrée standard
+   * - Envoyer la chaîne lue au serveur
+   */
+
+   return 0;
+}
---
+++ server_udp.cpp
@@ -0,0 +1,41 @@
+/*
+ * Code du serveur
+ */
+
+/* Port local du serveur */
+#define PORT 9600
+
+int main(int argc, char *argv[]) {
+
+  /*
+   * Variables du serveur
+   *
+   * Déclarer ici les variables suivantes :
+   * - sockfd le descripteur de socket
+   * - structure d'adresse locale du serveur
+   * - structure d'adresse du client
+   * - taille de l'adresse du client
+   *
+   */
+
+  /*
+   * Code du serveur
+   *
+   * - Ouvrir le socket du serveur
+   * - Remplir la structure d'adresse locale du serveur :
+   *   - la famille d'adresse
+   *   - l'adresse IP
+   *   - le port
+   *
+   * - Spécifier l'adresse locale du socket du serveur
+   */
+
+  while (1) {
+
+    /*
+     * Code de l'intérieur de la boucle
+     */
+  }
+
+  return 0;
+}
